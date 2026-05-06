786e174cbe97c3c94f2e62e1d2ef28d59eadcec6
Rebeca <rebecamartinezmedina@hotmail.com>
2026-05-06 20:56:58 +0200

    Errata 2

--- proyectoTiendaReact/src/pages/Auth/Perfil.jsx
+++ proyectoTiendaReact/src/pages/Auth/Perfil.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { useNavigate, Link } from "react-router-dom";
-import { useAuth } from "../context/AuthContext";
+import { useAuth } from "../../context/AuthContext";
 import logoEmpresa from "/Tienda_logo.png";
 
 export default function Perfil() {
