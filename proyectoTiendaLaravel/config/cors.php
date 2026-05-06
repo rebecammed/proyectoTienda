e3808bde16deba5c2abd50253317f4f7caae1b21
Rebeca <rebecamartinezmedina@hotmail.com>
2026-05-06 18:57:07 +0200

    Modificación CORS 2

--- proyectoTiendaLaravel/config/cors.php
+++ proyectoTiendaLaravel/config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173', 'https://proyecto-tienda-sigma.vercel.app/'],
+    'allowed_origins' => ['http://localhost:5173', 'https://proyecto-tienda-sigma.vercel.app'],
 
     'allowed_origins_patterns' => [],
 
