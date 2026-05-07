e1cd50a27e64eb7ea2a18d1acfc0eb1d26772e89
Rebeca <rebecamartinezmedina@hotmail.com>
2026-05-07 11:42:44 +0200

    Login 2

--- proyectoTiendaReact/src/pages/Auth/Login.jsx
+++ proyectoTiendaReact/src/pages/Auth/Login.jsx
@@ -8,7 +8,7 @@ const Login = () => {
   const [error, setError] = useState("");
   const [cargando, setCargando] = useState(false);
   const navigate = useNavigate();
-  const { login } = useAuth();
+  const { setAuthData } = useAuth();
 
   // Estilos unificados
   const buttonStyles = {
